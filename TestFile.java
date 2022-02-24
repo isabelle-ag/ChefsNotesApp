d271b824d3bc868bcc39017e2be002a9df2a4f55
EranEfron <eranefron@hotmail.com>
2022-02-23 23:45:36 -0600

    Created a temporary button on main view to switch to recipe creation/edit view, and added title space to recipe creation/edit view.

--- TestFile.java
+++
@@ -1,8 +0,0 @@
-public class TestFile
-{
-    public static void main(String args[])
-    {
-        System.out.println("Hello World");//testing editing on gitlab
-    }
-}
-
---
+++ app/src/main/java/comp3350/chefsnotes/EditRecipe.java
@@ -0,0 +1,6 @@
+package comp3350.chefsnotes;
+
+import android.app.Activity;
+
+public class EditRecipe extends Activity {
+}
