387aea28e5c9896ec5533e5a7255b05d8d82d7d5
Colin <johns233@myumanitoba.ca>
2022-02-24 15:27:37 -0600

    add and remove interface methods

--- app/src/main/java/comp3350/chefsnotes/DBMSTools.java
+++ app/src/main/java/comp3350/chefsnotes/DBMSTools.java
@@ -34,7 +34,7 @@ public interface DBMSTools{
     // updates a recipe to have a new name
     // fails if recipe DNE
     // returns true on success, false on failure
-    boolean updateRecipe(String recipe, String newName);
+    boolean updateRecipeName(String recipe, String newName);
 
     // creates a deep copy of the target recipe, with newName as its title. 
     // if newName = null, default is <oldName> + " - copy" + <copyNum>
@@ -62,15 +62,6 @@ public interface DBMSTools{
     // returns true on success, false on failure
     boolean addIngredient(String recipe, String ingName, int amount, String units);
 
-    // updates an existing ingredient's information -- CANNOT CHANGE NAME
-    // fails if ingredient DNE
-    // returns true on success, false on failure.
-    boolean updateIngredient(String recipe, String ingName, int amount, String units);
-
-    // updates an existing ingredient's name to newName
-    // fails if ingredient DNE
-    boolean updateIngredient(String recipe, String ingName, String newName);
-
     // returns true on success, false on failure
     boolean deleteIngredient(String recipe, String ingName);
 
@@ -84,10 +75,6 @@ public interface DBMSTools{
     // returns null if out of bounds
     Direction getDirection(String recipe, int dnum);
 
-    // returns single direction via title dname
-    // returns null if DNE
-    Direction getDirection(String recipe, String dname);
-
     // names the direction "Direction <num>" by default
     // returns new direction's number, -1 on failure
     int addDirection(String recipe, String text);
@@ -98,19 +85,6 @@ public interface DBMSTools{
     // addDirection but with name and time
     int addDirection(String recipe, String text, String dname, int time);
 
-    // sets the direction length
-    // returns false if DNE
-    boolean setTime(String recipe, int dnum, int time);
-
-    // updates Direction text
-    // returns true on success, false if DNE
-    boolean updateDirection(String recipe, int dnum, String newText);
-
-    // changes direction name
-    // returns false if direction name DNE or newName matches
-    // an existing direction name in recipe
-    boolean updateDirection(String recipe, String oldName, String newName);
-
     // reorders Directions. Target direction is moved to the new number,
     // and all other Directions are shifted linearly.
     // 0 < newNum < directionCount
@@ -120,10 +94,6 @@ public interface DBMSTools{
     // deletes the Direction numbered dnum
     // returns true on success, false if DNE
     boolean deleteDirection(String recipe, int dnum);
-
-    // deletes the Direction with title dname
-    // returns true on success, false if DNE
-    boolean deleteDirection(String recipe, String dname);
 
 
 
