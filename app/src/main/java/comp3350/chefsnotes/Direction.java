47ea2c9880f6db23bd3d41abd061e1f8c0ea0b2a
Colin <johns233@myumanitoba.ca>
2022-02-24 15:28:38 -0600

    create DSO classes and methods

--- app/src/main/java/comp3350/chefsnotes/Direction.java
+++ app/src/main/java/comp3350/chefsnotes/Direction.java
@@ -1,3 +1,51 @@
 public class Direction {
-    
+    private String name;
+    private String text;
+    private int time;
+
+    public Direction(String txt, String n, int t){
+        this.name = n;
+        this.text = txt;
+        this.time = t;
+    }
+
+    public Direction(String txt, int t){
+        this.name = "";
+        this.text = txt;
+        this.time = t;
+    }
+
+    public Direction(String txt, String n){
+        this.name = n;
+        this.text = txt;
+        this.time = 0;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String setName(String newName){
+        String result = this.name;
+        this.name = newName;
+        return result;
+    }
+
+    public String getText(){
+        return this.txt;
+    }
+
+    public void setText(String newTxt){
+        this.text = newTxt;
+    } 
+
+    public int getTime(){
+        return this.time;
+    }
+
+    public int setTime(int newTime){
+        int result = this.time;
+        this.time = newTime;
+        return result;
+    }
 }
--- app/src/main/java/comp3350/chefsnotes/Ingredient.java
+++ app/src/main/java/comp3350/chefsnotes/Ingredient.java
@@ -1,3 +1,50 @@
 public class Ingredient {
-    
+    private String name;
+    private Quantity amount;
+
+    public Ingredient(String iName, double a, String unit){
+        this.name = iName;
+        this.amount = new Quantity(a, unit);
+    }
+
+    public Ingredient(String iName, int numer, int denom, String unit){
+        this.name = iName;
+        this.amount = new Quantity(numer, denom, unit);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    // might be better off done differently. advise caution. edit if necessary.
+    // likely only way to perform Fraction Arithmetic properly though
+    public Quantity getAmt(){
+        return this.amount;
+    }
+
+    public String getAmtString(){
+        return this.amount.toString();
+    }
+
+    public void setAmt(double newAmt){
+        this.amount.setAmt(newAmt);
+    }
+
+    public void setAmt(int newNume, int newDenom){
+        this.amount.setAmt(newNume, newDenom);
+    }
+
+    public boolean setUnit(String newUnit){
+        this.amount.setUnit(newUnit);
+    }
+
+    public String toString(){
+        String result = "";
+        result = amount.toString() + " " + this.name;
+        return result;
+    }
 }
