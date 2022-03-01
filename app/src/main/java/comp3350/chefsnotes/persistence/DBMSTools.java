aecc1b08b16bdd715d56b1b2665f22108463eec9
kyle ondracek <ondracek@myumanitoba.ca>
2022-03-01 12:28:39 -0600

    rename

--- app/src/main/java/comp3350/chefsnotes/persistence/DBMSTools.java
+++ app/src/main/java/comp3350/chefsnotes/persistence/DBMSTools.java
@@ -1,8 +1,7 @@
-package comp3350.chefsnotes.persistance;
+package comp3350.chefsnotes.persistence;
 
 import comp3350.chefsnotes.objects.Direction;
 import comp3350.chefsnotes.objects.Ingredient;
-
 public interface DBMSTools{
     // Any DBMS implementing this interface are required to implement
     // a copy of the methods listed below.
