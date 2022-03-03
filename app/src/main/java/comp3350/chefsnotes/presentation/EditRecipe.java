0a67ce616a79cc300bfa8c4c2d36afd8f8532f95
danieldebassige <danieldebassige@gmail.com>
2022-03-03 16:05:19 -0600

    Added Listeners to recipe_editor UI
    Added Listeners to the save button, add ingredient field button, and add instruction field button.
    
    Listener for save button also gets data from all fields.

--- app/src/main/java/comp3350/chefsnotes/presentation/EditRecipe.java
+++ app/src/main/java/comp3350/chefsnotes/presentation/EditRecipe.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import comp3350.chefsnotes.R;
+import comp3350.chefsnotes.objects.Direction;
+import comp3350.chefsnotes.objects.Ingredient;
 
 public class EditRecipe extends AppCompatActivity {
 
@@ -14,19 +21,132 @@ public class EditRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_editor);
+
+        ImageButton saveButton = findViewById(R.id.save_button);
+        View addIngredientButton = findViewById(R.id.AddIngredientButton);
+        View addInstructionButton = findViewById(R.id.AddInstructionButton);
+
+        saveButton.setOnClickListener(v -> {
+            String title = getTitle(v);
+            ArrayList<Ingredient> ingredients = getIngredients(v);
+            ArrayList<Direction> directions = getInstructions(v);
+
+            if(ingredients != null && directions != null)
+            {
+                System.out.println("Saving " + title + "...");
+            }
+            else
+            {
+                System.out.println("Abort save, null field found.");
+            }
+        });
+
+        addInstructionButton.setOnClickListener(this::addInstruction);
+        addIngredientButton.setOnClickListener(this::addIngredient);
+
     }
 
     public void addInstruction(View view)
     {
-        LinearLayout instructionContainer = (LinearLayout) findViewById(R.id.InstructionContainer);
+        LinearLayout instructionContainer = findViewById(R.id.InstructionContainer);
         View child = getLayoutInflater().inflate(R.layout.instruction_field, null);
         instructionContainer.addView(child);
     }
 
     public void addIngredient(View view)
     {
-        LinearLayout ingredientContainer = (LinearLayout) findViewById(R.id.IngredientContainer);
+        LinearLayout ingredientContainer = findViewById(R.id.IngredientContainer);
         View child = getLayoutInflater().inflate(R.layout.ingredient_field, null);
         ingredientContainer.addView(child);
+    }
+
+    private String getTitle(View view)
+    {
+        return ((EditText) findViewById(R.id.recipeTitle)).getText().toString();
+    }
+
+    private ArrayList<Ingredient> getIngredients(View view)
+    {
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        Ingredient current;
+        LinearLayout container = findViewById(R.id.IngredientContainer);
+        View currRow;
+
+        for(int i = 0; i < container.getChildCount(); i++)
+        {
+            currRow = container.getChildAt(i);
+
+            String ingredientCount =
+                    ((EditText)currRow.findViewById(R.id.IngredientAmount))
+                            .getText().toString();
+
+            String ingredientName =
+                    ((EditText)currRow.findViewById(R.id.IngredientName))
+                            .getText().toString();
+
+            String ingredientUnit =
+                    ((Spinner)currRow.findViewById(R.id.UnitList))
+                            .getSelectedItem().toString();
+
+            if(!ingredientName.equals("") && !ingredientCount.equals("") && !ingredientUnit.equals(""))
+            {
+                current = new Ingredient(ingredientName, Double.parseDouble(ingredientCount), ingredientUnit);
+                ingredients.add(current);
+            }
+            else
+            {
+                // Create some kind of UI error here for empty field
+                System.out.println("Cannot Save ingredient! (Empty Field)");
+                return null;
+            }
+        }
+
+        return ingredients;
+    }
+
+    private ArrayList<Direction> getInstructions(View view)
+    {
+        ArrayList<Direction> directions = new ArrayList<>();
+        Direction current;
+        LinearLayout ingredients = findViewById(R.id.InstructionContainer);
+        View currRow;
+
+        for(int i = 0; i < ingredients.getChildCount(); i++)
+        {
+            currRow = ingredients.getChildAt(i);
+
+            String instructionName =
+                    ((EditText)currRow.findViewById(R.id.InstructionName))
+                            .getText().toString();
+
+            String instructions =
+                    ((EditText)currRow.findViewById(R.id.Instructions))
+                            .getText().toString();
+
+            String timeEstimate =
+                    ((EditText)currRow.findViewById(R.id.TimeEstimate))
+                            .getText().toString();
+
+            if(!instructions.equals(""))
+            {
+                if(timeEstimate.equals(""))
+                {
+                    timeEstimate = "0";
+                }
+                if(instructionName.equals("Name")) {
+                    instructionName = "";
+                }
+                current = new Direction(instructions, instructionName, Integer.parseInt(timeEstimate));
+                directions.add(current);
+            }
+            else
+            {
+                // Create some kind of UI warning about empty field
+                System.out.println("Cannot Save instruction!");
+                return null;
+            }
+        }
+
+        return directions;
     }
 }
