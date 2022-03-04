5833391dd3d4681b57992ed03c5a6b913ceb5e26
Isabelle Anderson-Gregoire <andersoi@myumanitoba.ca>
2022-03-03 18:15:34 -0600

    Added Functionality to Recipe Fetcher to pass to ViewRecipe

--- app/src/main/java/comp3350/chefsnotes/business/RecipeFetcher.java
+++ app/src/main/java/comp3350/chefsnotes/business/RecipeFetcher.java
@@ -1,24 +1,33 @@
 package comp3350.chefsnotes.business;
 
+import comp3350.chefsnotes.objects.Direction;
+import comp3350.chefsnotes.objects.Ingredient;
 import comp3350.chefsnotes.persistence.DBMSTools;
 import comp3350.chefsnotes.objects.Recipe;
 
-public class RecipeFetcher implements IRecipeFetcher{
+public class RecipeFetcher implements IRecipeFetcher {
     private final DBMSTools db;
+    private Recipe currentRecipe;
 
-    public RecipeFetcher(DBMSTools db)
-    {
+    public RecipeFetcher(DBMSTools db) {
         this.db = db;
     }
 
-    public Recipe getRecentRecipe()
-    {
+    public void setRecentRecipe(){
         Recipe[] allRecipes = db.getAllRecipes();
-        if(allRecipes.length < 1)
+        if(allRecipes.length >= 1)
         {
+            currentRecipe = allRecipes[(allRecipes.length-1)];
+        }
+        return;
+    }
+
+    public Recipe getRecentRecipe() {
+        setRecentRecipe();
+        if(currentRecipe == null){
             return null;
         }
-        return allRecipes[(allRecipes.length-1)];
+        return currentRecipe;
     }
 
     public Recipe getRecipeByName(String name)
@@ -26,4 +35,33 @@ public class RecipeFetcher implements IRecipeFetcher{
         return db.getRecipe(name);
     }
 
+    public String[] getIngredients(){
+        Ingredient[] ingArray = currentRecipe.getIngredients();
+        int n = ingArray.length;
+        String[] strArray = new String[n];
+        for(int i=0; i<n; i++){
+            strArray[i]= ingArray[i].getAmtString() + "\t" + ingArray[i].getName();
+        }
+        return strArray;
+    }
+
+    public String[] getDirections(){
+        Direction[] dirArray = currentRecipe.getDirections();
+        int n = dirArray.length;
+        String[] strArray = new String[n];
+        for(int i=0; i<n; i++){
+            strArray[i]= "Step " + i + " " + dirArray[i].getName() + "\n" + dirArray[i].getText();
+        }
+        return strArray;
+    }
+    public String getRecipeName(){
+        return(currentRecipe.getTitle());
+    }
+
+    public boolean validRecipe(){
+        if(currentRecipe == null){
+            return false;
+        }
+        return true;
+    }
 }
--- app/src/main/java/comp3350/chefsnotes/presentation/ViewRecipe.java
+++ app/src/main/java/comp3350/chefsnotes/presentation/ViewRecipe.java
@@ -5,44 +5,56 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.List;
 
 import comp3350.chefsnotes.R;
-import comp3350.chefsnotes.objects.Direction;
-import comp3350.chefsnotes.objects.Ingredient;
+import comp3350.chefsnotes.business.RecipeFetcher;
+import comp3350.chefsnotes.persistence.FakeDBMS;
 import comp3350.chefsnotes.objects.Recipe;
+
 import android.widget.ArrayAdapter;
 
 public class ViewRecipe extends AppCompatActivity {
-    private Recipe recipe;
-//    private Direction[] directionArray;
-//    private Ingredient[] ingredientList;
-//    private ArrayAdapter<Direction> directionAdapter;
-//    private ArrayAdapter<Ingredient> ingredientAdapter;
+    private RecipeFetcher recipeFetcher = new RecipeFetcher(new FakeDBMS());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recipe);
-        makeRec();
-        //this.rec = getRecipe();
-        fill();
+        recipeFetcher.setRecentRecipe();
+        if(recipeFetcher.validRecipe()) {
+            fillViewer();
+        }
+        else{
+            errorScreen();
+        }
 
     }
 
-    private void fill(){
-        final ListView listView = (ListView)findViewById(R.id.IngredientListView);
-        String title= "This is the recipe title!";
-        ((TextView)findViewById(R.id.recipeName)).setText(recipe.getTitle());
+    private void fillViewer(){
+        String [] directions = recipeFetcher.getDirections();
+        String [] ingredients = recipeFetcher.getIngredients();
+        String title = recipeFetcher.getRecipeName();
+
         ((TextView)findViewById(R.id.recipeName)).setText(title);
 
+        //Ingredient List
+        ArrayAdapter<String> ingAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, ingredients);
+        ListView ingView = (ListView) findViewById(R.id.ingredientListView);
+        ingView.setAdapter(ingAdapter);
+
+        //Direction List
+        ArrayAdapter<String> dirAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, directions);
+        ListView dirView = (ListView) findViewById(R.id.directionListView);
+        dirView.setAdapter(ingAdapter);
 
     }
 
-    private void makeRec(){
-        recipe = new Recipe("Temporary Recipe");
-        recipe.addIngredient("Ing1", 5, "unit");
-        recipe.addDirection("Here's a direction!");
+    private void errorScreen(){
+        String error = "There are no recipes at the moment.";
+
+        ((TextView)findViewById(R.id.recipeName)).setText(error);
     }
 
 
