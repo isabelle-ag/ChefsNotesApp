8abb8ada506a32f98e1b28c7eb2e3aeb2aa22317
kyle ondracek <ondracek@myumanitoba.ca>
2022-03-28 13:16:17 -0500

    add testGetRecipesByText

--- app/src/test/java/comp3350/chefsnotes/business/IRecipeFetcherTest.java
+++ app/src/test/java/comp3350/chefsnotes/business/IRecipeFetcherTest.java
@@ -6,7 +6,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import comp3350.chefsnotes.R;
+import comp3350.chefsnotes.objects.Recipe;
 import comp3350.chefsnotes.persistence.DBMSTools;
 import comp3350.chefsnotes.persistence.FakeDBMS;
 
@@ -19,7 +23,11 @@ public class IRecipeFetcherTest {
         db = new FakeDBMS();
         db.createRecipe("test1");
         db.createRecipe("test2");
+        db.createRecipe("foo");
+        db.createRecipe("barfoo");
+        db.createRecipe("bar");
         db.createRecipe("testRECENT");
+
         fetcher = new RecipeFetcher(db);
 
     }
@@ -38,5 +46,14 @@ public class IRecipeFetcherTest {
 
         assertEquals(fetcher.getRecipeByName("test1").getTitle(), "test1");
 
+    }
+
+    @Test
+    public void testGetRecipesByText() {
+        ArrayList<Recipe> fetched = new ArrayList<Recipe>(Arrays.asList(fetcher.getRecipesByText("foo")));
+
+        assertTrue(fetched.contains(db.getRecipe("foo")));
+        assertTrue(fetched.contains(db.getRecipe("barfoo")));
+        assertFalse(fetched.contains(db.getRecipe("bar")));
     }
 }
