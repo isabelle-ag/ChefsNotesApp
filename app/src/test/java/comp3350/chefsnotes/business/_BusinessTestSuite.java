611cd0ab2ce95287440ca8273e545fcb812d187d
kyle ondracek <ondracek@myumanitoba.ca>
2022-04-22 20:25:24 -0500

    more test coverage

--- app/src/test/java/comp3350/chefsnotes/business/_BusinessTestSuite.java
+++ app/src/test/java/comp3350/chefsnotes/business/_BusinessTestSuite.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         IRecipeManagerTest.class,
         IRecipeFetcherTest.class,
-        ITagHandlerTest.class
+        ITagHandlerTest.class,
+        UnitsTest.class
 })
 
 public class _BusinessTestSuite {
