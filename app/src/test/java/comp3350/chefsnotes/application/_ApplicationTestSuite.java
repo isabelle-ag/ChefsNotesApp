886224f69cb24eeb30f0e8835af85c5318f902ba
Colin Johnson <johns233@myumanitoba.ca>
2022-04-23 18:50:48 -0500

    application test package

---
+++ app/src/test/java/comp3350/chefsnotes/application/_ApplicationTestSuite.java
@@ -0,0 +1,14 @@
+package comp3350.chefsnotes.application;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        MainTest.class,
+        ServicesTest.class
+})
+
+public class _ApplicationTestSuite {
+}
